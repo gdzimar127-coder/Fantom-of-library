76feb8625ff63d5665a7dce8df65d2bfc3bc3931
gdzimar127-coder <gdzimar127@gmail.com>
2026-01-29 18:21:21 +0300

    набор тайлов
    набор тайлов в загрузку

---
+++ fon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="fon" tilewidth="70" tileheight="70" tilecount="406" columns="14">
+ <image source="../Desktop/images for project/фон/medieval_spritesheet.png" width="1024" height="2048"/>
+</tileset>
